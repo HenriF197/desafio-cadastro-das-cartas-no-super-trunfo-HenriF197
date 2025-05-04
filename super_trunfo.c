52eb4102ed7af422e3a482523ee269e86ad3f179
HenriF197 <hnrqff1948@outlook.com>
2025-05-04 19:16:49 -0300

    Update super_trunfo.c
    comparando valores para descobrir quem vence ou perde

--- super_trunfo.c
+++ super_trunfo.c
@@ -8,7 +8,7 @@ int main () {
     char codigo[10] = "A01";
     char estado[20] = "Alagoas";
     char capital[20] = "maceió";
-    int populacao = 900;
+    unsigned long int populacao = 90;
     float area = 509.33;
     // área em km²
 
@@ -23,20 +23,19 @@ int main () {
     printf ("Código da cidade: %s \n", codigo);
     printf ("Nome do Estado: %s \n", estado);
     printf ("Capital: %s \n", capital);
-    printf ("População: %d mil \n", populacao);
+    printf ("População: %lu milhões \n", populacao);
     printf ("Área: %.2f km² \n", area);
     printf ("PIB: %.2f milhões \n", PIB);
     printf ("Quantidade Pontos Turísticos: %d \n", pontosTuristicos);
 
    //calculo populacional e PIB usando divisao
     printf ("Densidade Populacional: %.2f km² \n", populacao / area);
-    printf ("PIB per Capta: %.2f reais \n", PIB / populacao);
+    printf ("PIB per Capita: %.2f reais \n", PIB / populacao);
+
+    printf ("Super Poder: %.2f \n", pontosTuristicos + PIB + populacao + populacao / area + PIB / populacao);
 
 
-
-
-   
-
+    
 
     char InicialEstado = 'A';
     //inicial do estado
@@ -44,7 +43,7 @@ int main () {
     char Codigo[10] = "A02";
     char Estado[20] = "Amazônia";
     char Capital[20] = "Manaus";
-    int Populacao = 2.10;
+    unsigned long int Populacao = 25;
     float Area = 11.35;
     // área em km²
 
@@ -57,17 +56,27 @@ int main () {
     printf ("Código da cidade: %s \n", Codigo);
     printf ("Nome do Estado: %s \n", Estado);
     printf ("Capital: %s \n", Capital);
-    printf ("População: %d milhões \n", Populacao);
+    printf ("População: %lu milhões \n", Populacao);
     printf ("Área: %.2f km² \n", Area);
     printf ("PIB: %.2f milhões \n", PiB);
-    printf ("Quantidade de Pontos Turísticos: %d", PontosTuristicos);
+    printf ("Quantidade de Pontos Turísticos: %d\n", PontosTuristicos);
    
 
     printf ("Densidade Populacional: %.2f km² \n", Populacao / Area);
-    printf ("PIB per Capta: %.2f reais \n", PiB / Populacao);
+    printf ("PIB per Capita: %.2f reais \n", PiB / Populacao);
   
 
-   printf ("Super Poder: %.2f /n", (populacao / area + PiB / populacao));
+   printf ("Super Poder: %.2f \n", PontosTuristicos + PiB + Populacao + Populacao / Area + PiB / Populacao);
+
+
+   printf ("População A > População B: %d Venceu \n", populacao > Populacao);
+   printf ("Area A > Area B: %d Venceu \n", area > Area);
+   printf ("PIB A > PIB B: %d Venceu \n", PIB > PiB);
+   printf ("Pontos Turisticos A > Pontos Turisticos B: %d Venceu \n", pontosTuristicos > PontosTuristicos);
+   printf ("Densidade Populacional A > Densidade Populacional B: %d Venceu \n", populacao / area > Populacao / Area);
+   printf ("PIB per Capita A > PIB per Capita B: %d Venceu \n", PIB / populacao > PiB / Populacao);
+   printf ("Super Poder A > Super Poder B: %d Venceu \n", pontosTuristicos + PIB + populacao + populacao / area + PIB / populacao > PontosTuristicos + PiB + Populacao + Populacao / Area + PiB / Populacao);
+
  
 
 
